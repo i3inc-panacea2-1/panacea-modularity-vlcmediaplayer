6b88adfab82ef7e8fad8b42fb7b4a51266b6d4e3
Portno <j.remoundos@dotbydot.gr>
2019-05-19 12:27:42 +0300

    first commit

---
+++ src/Panacea.Modularity.VlcMediaPlayer/IVlcBinariesPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.VlcMediaPlayer
+{
+    public interface IVlcBinariesPlugin : IPlugin
+    {
+        string GetVersion();
+        string GetBinariesPath();
+    }
+}
